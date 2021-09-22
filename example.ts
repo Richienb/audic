42b8741f5a7c29622504162ace539779e9cc7c2c
Richie Bendall <richiebendall@gmail.com>
2021-09-23 01:32:26 +1200

    Meta tweaks

--- example.ts
+++ example.ts
@@ -1,3 +1,4 @@
 import {playAudioFile} from './source/index.js';
 
-await playAudioFile('./fixtures/Moonlight (second movement).ogg');
+// Audio from https://commons.wikimedia.org/wiki/File:Beethoven_Moonlight_1st_movement.ogg
+await playAudioFile('./fixtures/Moonlight Sonata (1st movement).ogg');
